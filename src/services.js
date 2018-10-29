4e3840073bee39e2a3b1c0807b876572ef9c8fa8
Adrian Mustea <mustea.adrian95@gmail.com>
2018-10-29 08:57:53 +0200

    init commit

---
+++ src/services.js
@@ -0,0 +1,28 @@
+const services = [
+  {
+    title: "webdesign",
+    headline: "pixel perfect",
+    description:
+      "As a design-centric studio, we develop responsive websites, that are pleasing to the eye in any device. ",
+    img: "webdesign.svg",
+    color: "var(--blue)"
+  },
+  {
+    title: "static websites",
+    headline: "target reaching",
+    description:
+      "Fast, modern, responsive websites. From landing pages to fully fledged web applications.",
+    img: "ecommerce.svg",
+    color: "var(--pink)"
+  },
+  {
+    title: "branding identity",
+    headline: "unique",
+    description:
+    "Just like your personal identity makes you uniquely you, your brand identity is the special sauce of your business that sets you apart from every other Tom, Dick and Harry, Inc. on the block.",
+    img: "ecommerce.svg",
+    color: "var(--yellow)"
+  }
+];
+
+export default services;
